0b69458005a4cabde1b882e9b92f677935cfecff
AnuragJha003 <97457228+AnuragJha003@users.noreply.github.com>
2023-03-09 16:05:15 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,3 +21,21 @@ public:
 
 
 https://leetcode.com/problems/binary-search/
+
+
+//ON A REVERSE SORTED ARRAY 
+while(s<=e){
+    int mid=s+(e-s)/2;
+    if(mid==target)return mid;
+    else if (target<mid){
+        s=mid+1;
+    }
+    else{
+        e=mid-1;
+    }
+}
+return -1;
+
+
+//IF ORDER NOT KNOWN ::THEN ARR[0]<ARR[1];;;ASCENDING BS ELSE DESCENDING BS
+
