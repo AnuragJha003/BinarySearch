593c791b672fa37f1cb1092f00ca20e7cc0edc25
AnuragJha003 <97457228+AnuragJha003@users.noreply.github.com>
2023-03-11 13:58:34 +0530

    Update KthElementInSortedArray.cpp

--- KthElementInSortedArray.cpp
+++ KthElementInSortedArray.cpp
@@ -19,3 +19,5 @@ int kthElement(int arr1[], int arr2[], int n, int m, int k){
     
     https://practice.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=k-th-element-of-two-sorted-array
     
+
+APPEROACH2:USE TWO POINTERS AND FORM A SINGLE SORTED ARRAY AND RETURN THE KTH INDEX O(N) APPROACH
